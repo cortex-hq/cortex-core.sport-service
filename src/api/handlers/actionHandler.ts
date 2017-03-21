8a52362a9c208101f67c24e442bf7c18de2aa27a
Pascal Recchia <precchia@sovinty.com>
2017-03-21 09:41:56 +0100

    Provides mvp (?) sport service

--- src/api/handlers/actionHandler.ts
+++ src/api/handlers/actionHandler.ts
@@ -13,17 +13,19 @@ export class SportActionHandler extends DefaultActionHandler {
 @ActionHandler({ async: false, scope: "?", schema: 'Season' })
 export class SeasonActionHandler extends DefaultActionHandler {
 
-
-
-    // /api/season.updategame
-    @Action({ description: "Provides way to update a game (Score, Incidents)", outputSchema: "string" }) // action = method name (minus Async)
+    //api/season.updategame
+    @Action({ description: "Provides way to update a game (Score, Incidents)", outputSchema: "Season" }) // action = method name (minus Async)
     async updateGame(game: Game) {
 
-        const cmd = await this.requestContext.getCommandAsync("UpdateGameCommand", this.metadata.schema);
-        await cmd.executeAsync(game);
-        return 'echo';
+        let handler = this.container.get<SeasonQueryHandler>('SeasonQueryService');
+        let season1 = await handler.getAsync(game.seasonId);
+        let gameIndex = season1.games.findIndex(g => g.id === game.id);
+        season1.games.splice(gameIndex, 1, game);
+        let season2 = await this.updateAsync(season1);
+        return season2;
     }
 
+    //api/season.reportInjury
     @Action({ description: "Provides way to report an injury", outputSchema: "string" }) // action = method name (minus Async)
     async reportInjury(injuryReport: InjuryReport) {
 
